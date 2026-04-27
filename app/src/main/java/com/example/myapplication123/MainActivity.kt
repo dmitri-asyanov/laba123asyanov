6dacab6a16748c79cb27bf4fd2140f29b5425fa9
dmitri-asyanov <qwertyuytrewq204@gmail.com>
2026-04-27 15:17:34 +0300

    1.lab1

--- app/src/main/java/com/example/myapplication123/MainActivity.kt
+++ app/src/main/java/com/example/myapplication123/MainActivity.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication123.ui.theme.MyApplication123Theme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplication123Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    ShoppingListElement("description")
                 }
             }
         }
@@ -31,17 +44,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ShoppingListElement(description: String) {
+    Text(description)
+}
+@Composable
+fun App() {
+    val someList = remember { listOf("Молоко", "Яйца", "Хлеб") }
+    LazyColumn {
+        items(someList) {
+            ShoppingListElement(it)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplication123Theme {
-        Greeting("Android")
+        App()
     }
 }
