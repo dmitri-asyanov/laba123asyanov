262048ecd1aa0bae6a6932e80f03e4a1a40b76df
dmitri-asyanov <qwertyuytrewq204@gmail.com>
2026-04-27 15:47:46 +0300

    2.lab2

--- app/src/main/java/com/example/myapplication123/MainActivity.kt
+++ app/src/main/java/com/example/myapplication123/MainActivity.kt
@@ -13,14 +13,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -36,23 +49,68 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplication123Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    ShoppingListElement("description")
+                    App()
                 }
             }
         }
     }
 }
 
-@Composable
-fun ShoppingListElement(description: String) {
-    Text(description)
-}
+data class ShoppingListItem(
+    val description: String,
+    val bought: Boolean = false
+)
+
 @Composable
 fun App() {
-    val someList = remember { listOf("Молоко", "Яйца", "Хлеб") }
+    val shoppingList = remember {
+        mutableStateListOf(ShoppingListItem("Молоко"), ShoppingListItem("Мука"))
+    }
+    var newItemDesc by remember { mutableStateOf("") }
     LazyColumn {
-        items(someList) {
-            ShoppingListElement(it)
+        item {
+            OutlinedTextField(
+                value = newItemDesc, onValueChange = { newItemDesc = it },
+                modifier = Modifier.padding(8.dp),
+                label = {
+                    Text("Название продукта")
+                },
+                trailingIcon = {
+                    IconButton(onClick = {
+                        if (newItemDesc.isNotBlank()) {
+                            shoppingList.add(ShoppingListItem(newItemDesc.trim()))
+                            newItemDesc = ""
+                        }
+                    }) {
+                        Icon(Icons.Default.Add, contentDescription = "Добавить")
+                    }
+                })
+        }
+        itemsIndexed(shoppingList) { i, item ->
+            ShoppingListElement(
+                item,
+                onBoughtChange = {
+                    shoppingList[i] = item.copy(bought = it)
+                },
+                onDelete = {
+                    shoppingList.removeAt(i)
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun ShoppingListElement(item: ShoppingListItem, onBoughtChange: (Boolean) -> Unit, onDelete: () -> Unit) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(
+            checked = item.bought,
+            onCheckedChange = onBoughtChange
+        )
+        Text(item.description, Modifier.weight(1f))
+        IconButton(onClick = onDelete) {
+            // contentDescription не отображается на экране, но читается средствами помощи слепым
+            Icon(Icons.Default.Delete, contentDescription = "Удалить")
         }
     }
 }
